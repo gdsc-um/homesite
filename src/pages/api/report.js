220640207ba0dbc0a8318eb7c33fd80bd1c7ac75
vzrenggamani <rengga.prakoso@gmail.com>
2023-06-03 03:11:18 +0700

    chore: formatting code as prettier says

--- src/pages/api/report.js
+++ src/pages/api/report.js
@@ -1,65 +1,82 @@
-import { google } from "googleapis"
+import { google } from "googleapis";
 
 const auth = new google.auth.GoogleAuth({
-    credentials: {
-        client_email: process.env.CLIENT_EMAIL,
-        client_id: process.env.CLIENT_ID,
-        private_key: process.env.PRIVATE_KEY,
-    },
-    scopes: [
-        'https://www.googleapis.com/auth/drive',
-        'https://www.googleapis.com/auth/drive.file',
-        'https://www.googleapis.com/auth/spreadsheets',
-    ],
+  credentials: {
+    client_email: process.env.CLIENT_EMAIL,
+    client_id: process.env.CLIENT_ID,
+    private_key: process.env.PRIVATE_KEY,
+  },
+  scopes: [
+    "https://www.googleapis.com/auth/drive",
+    "https://www.googleapis.com/auth/drive.file",
+    "https://www.googleapis.com/auth/spreadsheets",
+  ],
 });
 
 const sheets = google.sheets({
-    auth,
-    version: 'v4',
+  auth,
+  version: "v4",
 });
 
 export default async function handler(req, res) {
+  if (req.method === "POST") {
+    try {
+      // Get the data from the request body
+      const data = req.body;
+      var report_details = [];
+      var questions = data.data;
 
-    if (req.method === 'POST') {
-        try {
-            
-            // Get the data from the request body
-            const data = req.body;
-            var report_details = [];
-            var questions = data.data;
+      for (var i = 0; i < questions.length; i++) {
+        var question = questions[i];
+        var report = [
+          question.timestamp,
+          data.attempt,
+          data.quiz.uuid,
+          data.quiz.title,
+          question.uuid,
+          question.question,
+          question.userAnswer,
+          question.correctAnswer,
+          question.correct == true ? "Correct" : "Incorrect",
+        ];
+        report_details.push(report);
+      }
 
-            for (var i = 0; i < questions.length; i++) {
-                var question = questions[i];
-                var report = [question.timestamp, data.attempt, data.quiz.uuid, data.quiz.title, question.uuid, question.question, question.userAnswer, question.correctAnswer, question.correct == true ? "Correct" : "Incorrect"];
-                report_details.push(report);
-            }
+      var report_attempt = [
+        data.attempt,
+        data.quiz.uuid,
+        data.quiz.title,
+        data.score.correct,
+        data.score.incorrect,
+        data.totalQuestions,
+        data.startTime,
+        data.endTime,
+        data.duration,
+      ];
 
-            var report_attempt = [data.attempt, data.quiz.uuid, data.quiz.title, data.score.correct, data.score.incorrect, data.totalQuestions, data.startTime, data.endTime, data.duration];
+      await sheets.spreadsheets.values.append({
+        spreadsheetId: process.env.SPREADSHEET_ID,
+        range: "Analysis!A2:I",
+        valueInputOption: "USER_ENTERED",
+        insertDataOption: "INSERT_ROWS",
+        requestBody: {
+          values: report_details,
+        },
+      });
 
-            await sheets.spreadsheets.values.append({
-                spreadsheetId: process.env.SPREADSHEET_ID,
-                range: 'Analysis!A2:I',
-                valueInputOption: 'USER_ENTERED',
-                insertDataOption: 'INSERT_ROWS',
-                requestBody: {
-                    values: report_details
-                },
-            });
-
-            await sheets.spreadsheets.values.append({
-                spreadsheetId: process.env.SPREADSHEET_ID,
-                range: 'Attempts!A3:I',
-                valueInputOption: 'USER_ENTERED',
-                insertDataOption: 'INSERT_ROWS',
-                requestBody: {
-                    values: [report_attempt]
-                },
-            });
-            // Send a success response
-            res.status(200).json({ message: 'Data stored successfully' });
-        } catch (error) {
-            res.status(500).json({ message: 'Error storing data:' + error.message });
-        }
+      await sheets.spreadsheets.values.append({
+        spreadsheetId: process.env.SPREADSHEET_ID,
+        range: "Attempts!A3:I",
+        valueInputOption: "USER_ENTERED",
+        insertDataOption: "INSERT_ROWS",
+        requestBody: {
+          values: [report_attempt],
+        },
+      });
+      // Send a success response
+      res.status(200).json({ message: "Data stored successfully" });
+    } catch (error) {
+      res.status(500).json({ message: "Error storing data:" + error.message });
     }
-
+  }
 }
--- src/pages/quiz/[slug].jsx
+++ src/pages/quiz/[slug].jsx
@@ -1,190 +1,205 @@
-import React from 'react'
-import Link from 'next/link';
+import React from "react";
+import Link from "next/link";
 import { v4 } from "uuid";
 
 // Server-only code.
-const fs = require('fs')
+const fs = require("fs");
 export async function getStaticPaths() {
-    const files = fs.readdirSync('quizzes');
-    const paths = files.map((fileName) => ({
-        params: {
-        slug: fileName.replace('.json', ''),
-        },
-    }));
+  const files = fs.readdirSync("quizzes");
+  const paths = files.map((fileName) => ({
+    params: {
+      slug: fileName.replace(".json", ""),
+    },
+  }));
 
-        
-    // return file path
-    return {
-        paths,
-        fallback: false,
-    };
+  // return file path
+  return {
+    paths,
+    fallback: false,
+  };
 }
 
 export function getStaticProps({ params: { slug } }) {
-    // read the file questions from the quizzes folder
-    const questions = fs.readFileSync(`quizzes/${slug}.json`, 'utf8');
+  // read the file questions from the quizzes folder
+  const questions = fs.readFileSync(`quizzes/${slug}.json`, "utf8");
 
-    // return the questions data as props
-    return {
-        props: {
-            data: JSON.parse(questions),
-        }
-    }
+  // return the questions data as props
+  return {
+    props: {
+      data: JSON.parse(questions),
+    },
+  };
 }
 
 const Quiz = (questions) => {
-    const [currentQuestion, setCurrentQuestion] = React.useState(0);
-    const [score, setScore] = React.useState(0);
-    const [showScore, setShowScore] = React.useState(false);
+  const [currentQuestion, setCurrentQuestion] = React.useState(0);
+  const [score, setScore] = React.useState(0);
+  const [showScore, setShowScore] = React.useState(false);
 
-    const [answers, setAnswers] = React.useState([]);
-    const [startTime, setStartTime] = React.useState(0);
-    const [endTime, setEndTime] = React.useState(0);
-    const [sessionUUID, setSessionUUID] = React.useState(0);
+  const [answers, setAnswers] = React.useState([]);
+  const [startTime, setStartTime] = React.useState(0);
+  const [endTime, setEndTime] = React.useState(0);
+  const [sessionUUID, setSessionUUID] = React.useState(0);
 
-    // set start time for the first time
-    React.useEffect(() => {
-        if (startTime === 0) {
-            setStartTime(new Date(Date.now()));
-        }
-        setSessionUUID(v4());
-    }, []);
+  // set start time for the first time
+  React.useEffect(() => {
+    if (startTime === 0) {
+      setStartTime(new Date(Date.now()));
+    }
+    setSessionUUID(v4());
+  }, []);
 
-    const sendPOSTReport = async (report, url) => {
-        const response = await fetch(url, {
-            method: 'POST',
-            headers: {
-            'Content-Type': 'application/json'
-            },
-            body: JSON.stringify(report)
-        });
-        const data = await response.json();
-        console.log(data);
+  const sendPOSTReport = async (report, url) => {
+    const response = await fetch(url, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(report),
+    });
+    const data = await response.json();
+    console.log(data);
+  };
+
+  // function to handle every question
+  const handleQuestion = (answer) => {
+    var correct = false;
+    // set the end time
+    setEndTime(new Date(Date.now()));
+
+    // check if the answer is correct
+    if (answer.isCorrect) {
+      setScore(score + 1);
+      correct = true;
+    } else {
+      correct = false;
     }
 
-    // function to handle every question 
-    const handleQuestion = (answer) => {
-        var correct = false;
-        // set the end time
-        setEndTime(new Date(Date.now()));
+    var currentDate = new Date(Date.now());
+    answers.push({
+      uuid: questions.data.questions[currentQuestion].uuid,
+      question: questions.data.questions[currentQuestion].question,
+      userAnswer: answer.answer,
+      correctAnswer: questions.data.questions[currentQuestion].options.filter(
+        (option) => option.isCorrect,
+      )[0].answer,
+      correct: correct,
+      timestamp: `${currentDate.toLocaleDateString()} ${currentDate.toLocaleTimeString()}`,
+    });
 
-        
-        // check if the answer is correct
-        if (answer.isCorrect) {
-            setScore(score + 1);
-            correct = true;
-        } else {
-            correct = false;
-        }
+    // check if the current question is the last question
+    if (currentQuestion + 1 === questions.data.questions.length) {
+      // generate report data
+      const report = {
+        attempt: sessionUUID,
+        quiz: {
+          uuid: questions.data.metadata.uuid,
+          title: questions.data.metadata.title,
+        },
+        score: {
+          correct: score,
+          incorrect: questions.data.questions.length - score,
+        },
+        totalQuestions: questions.data.questions.length,
+        startTime: `${startTime.toLocaleDateString()} ${startTime.toLocaleTimeString()}`,
+        endTime: `${endTime.toLocaleDateString()} ${endTime.toLocaleTimeString()}`,
+        duration: Math.floor((endTime - startTime) / 1000 / 60),
+        data: answers,
+      };
 
-        var currentDate = new Date(Date.now());
-        answers.push({
-            'uuid': questions.data.questions[currentQuestion].uuid,
-            'question': questions.data.questions[currentQuestion].question,
-            'userAnswer': answer.answer,
-            'correctAnswer': questions.data.questions[currentQuestion].options.filter((option) => option.isCorrect)[0].answer,
-            'correct': correct,
-            'timestamp': `${currentDate.toLocaleDateString()} ${currentDate.toLocaleTimeString()}`,
-        });
+      // save the report
+      sendPOSTReport(report, "/api/report");
 
-        // check if the current question is the last question
-        if (currentQuestion + 1 === questions.data.questions.length) {
-            // generate report data
-            const report = {
-                'attempt': sessionUUID,
-                'quiz': {
-                    'uuid': questions.data.metadata.uuid,
-                    'title': questions.data.metadata.title,
-                },
-                'score': {
-                    'correct': score,
-                    'incorrect': questions.data.questions.length - score,
-                },
-                'totalQuestions': questions.data.questions.length,
-                'startTime': `${startTime.toLocaleDateString()} ${startTime.toLocaleTimeString()}`,
-                'endTime': `${endTime.toLocaleDateString()} ${endTime.toLocaleTimeString()}`,
-                'duration': Math.floor((endTime - startTime) / 1000 / 60),
-                'data': answers,
-            };
+      // if it is the last question, show the score
+      setShowScore(true);
+      return;
+    }
 
-            // save the report
-            sendPOSTReport(report, '/api/report');
+    // set the next question
+    const nextQues = currentQuestion + 1;
+    nextQues < questions.data.questions.length && setCurrentQuestion(nextQues);
+  };
 
-            // if it is the last question, show the score
-            setShowScore(true);
-            return;
-        }
-
-        // set the next question
-        const nextQues = currentQuestion + 1;
-        nextQues < questions.data.questions.length && setCurrentQuestion(nextQues);
-    };
-
-    // return the quiz page
-    return (
-        <div className='w-full min-h-screen flex flex-col'>
-            {
-                showScore ? (
-                    <div className='flex justify-center items-center w-full min-h-screen'>
-                        <h1 className="text-3xl font-semibold text-center text-[#262626]">
-                            You scored {score} out of {questions.data.questions.length}
-                        </h1>
-                    </div>
-                ) : (
-                    <>
-                        <div className='w-full h-[40vh] px-10 bg-white flex justify-center items-center relative'>
-                            <div className='absolute top-5 left-5 px-5 py-3 bg-neutral-100 rounded shadow-md'>
-                                <h3 className='text-[#262626] text-[1vw] font-semibold'>
-                                    Question {currentQuestion + 1}
-                                </h3>
-                            </div>
-                            <Link href='/quiz'>
-                                <div className='absolute top-5 right-5 px-5 py-3 bg-neutral-100 rounded shadow active:scale-90 duration-200 lg:hover:scale-105 lg:hover:shadow-md'>
-                                    <h3 className='text-[#262626] text-[1vw] font-semibold'>
-                                        Back to Quizzes
-                                    </h3>
-                                </div>
-                            </Link>
-                            <h2 className='text-start text-[1.6vw] text-[#262626] whitespace-pre-line'>
-                                {questions.data.questions[currentQuestion].question}
-                            </h2>
-                        </div>
-                        <div className='w-full h-[60vh] flex justify-center items-center'>
-                            <div className=" grid grid-cols-2 gap-8 gap-x-12">
-                                <button onClick={
-                                    () => handleQuestion(questions.data.questions[currentQuestion].options[0])
-                                }>
-                                    <div className="w-[35vw] h-[15vh] rounded-md flex justify-center items-center text-[#262626] text-[1.7vw] font-semibold bg-white hover:cursor-pointer lg:hover:shadow-lg shadow lg:hover:scale-105 lg:hover:-translate-y-2 duration-200 active:scale-90">
-                                        {questions.data.questions[currentQuestion].options[0].answer}
-                                    </div>
-                                </button>
-                                <button onClick={
-                                    () => handleQuestion(questions.data.questions[currentQuestion].options[1])
-                                }>
-                                    <div className="w-[35vw] h-[15vh] rounded-md flex justify-center items-center text-[#262626] text-[1.7vw] font-semibold bg-white hover:cursor-pointer lg:hover:shadow-lg shadow lg:hover:scale-105 lg:hover:-translate-y-2 duration-200 active:scale-90">
-                                        {questions.data.questions[currentQuestion].options[1].answer}
-                                    </div>
-                                </button>
-                                <button onClick={
-                                    () => handleQuestion(questions.data.questions[currentQuestion].options[2])
-                                }>
-                                    <div className="w-[35vw] h-[15vh] rounded-md flex justify-center items-center text-[#262626] text-[1.7vw] font-semibold bg-white hover:cursor-pointer lg:hover:shadow-lg shadow lg:hover:scale-105 lg:hover:-translate-y-2 duration-200 active:scale-90">
-                                        {questions.data.questions[currentQuestion].options[2].answer}
-                                    </div>
-                                </button>
-                                <button onClick={
-                                    () => handleQuestion(questions.data.questions[currentQuestion].options[3])
-                                }>
-                                    <div className="w-[35vw] h-[15vh] rounded-md flex justify-center items-center text-[#262626] text-[1.7vw] font-semibold bg-white hover:cursor-pointer lg:hover:shadow-lg shadow lg:hover:scale-105 lg:hover:-translate-y-2 duration-200 active:scale-90">
-                                        {questions.data.questions[currentQuestion].options[3].answer}
-                                    </div>
-                                </button>
-                            </div>
-                        </div>
-                    </>)
-            }
+  // return the quiz page
+  return (
+    <div className="w-full min-h-screen flex flex-col">
+      {showScore ? (
+        <div className="flex justify-center items-center w-full min-h-screen">
+          <h1 className="text-3xl font-semibold text-center text-[#262626]">
+            You scored {score} out of {questions.data.questions.length}
+          </h1>
         </div>
-    );
-}; 
+      ) : (
+        <>
+          <div className="w-full h-[40vh] px-10 bg-white flex justify-center items-center relative">
+            <div className="absolute top-5 left-5 px-5 py-3 bg-neutral-100 rounded shadow-md">
+              <h3 className="text-[#262626] text-[1vw] font-semibold">
+                Question {currentQuestion + 1}
+              </h3>
+            </div>
+            <Link href="/quiz">
+              <div className="absolute top-5 right-5 px-5 py-3 bg-neutral-100 rounded shadow active:scale-90 duration-200 lg:hover:scale-105 lg:hover:shadow-md">
+                <h3 className="text-[#262626] text-[1vw] font-semibold">
+                  Back to Quizzes
+                </h3>
+              </div>
+            </Link>
+            <h2 className="text-start text-[1.6vw] text-[#262626] whitespace-pre-line">
+              {questions.data.questions[currentQuestion].question}
+            </h2>
+          </div>
+          <div className="w-full h-[60vh] flex justify-center items-center">
+            <div className=" grid grid-cols-2 gap-8 gap-x-12">
+              <button
+                onClick={() =>
+                  handleQuestion(
+                    questions.data.questions[currentQuestion].options[0],
+                  )
+                }
+              >
+                <div className="w-[35vw] h-[15vh] rounded-md flex justify-center items-center text-[#262626] text-[1.7vw] font-semibold bg-white hover:cursor-pointer lg:hover:shadow-lg shadow lg:hover:scale-105 lg:hover:-translate-y-2 duration-200 active:scale-90">
+                  {questions.data.questions[currentQuestion].options[0].answer}
+                </div>
+              </button>
+              <button
+                onClick={() =>
+                  handleQuestion(
+                    questions.data.questions[currentQuestion].options[1],
+                  )
+                }
+              >
+                <div className="w-[35vw] h-[15vh] rounded-md flex justify-center items-center text-[#262626] text-[1.7vw] font-semibold bg-white hover:cursor-pointer lg:hover:shadow-lg shadow lg:hover:scale-105 lg:hover:-translate-y-2 duration-200 active:scale-90">
+                  {questions.data.questions[currentQuestion].options[1].answer}
+                </div>
+              </button>
+              <button
+                onClick={() =>
+                  handleQuestion(
+                    questions.data.questions[currentQuestion].options[2],
+                  )
+                }
+              >
+                <div className="w-[35vw] h-[15vh] rounded-md flex justify-center items-center text-[#262626] text-[1.7vw] font-semibold bg-white hover:cursor-pointer lg:hover:shadow-lg shadow lg:hover:scale-105 lg:hover:-translate-y-2 duration-200 active:scale-90">
+                  {questions.data.questions[currentQuestion].options[2].answer}
+                </div>
+              </button>
+              <button
+                onClick={() =>
+                  handleQuestion(
+                    questions.data.questions[currentQuestion].options[3],
+                  )
+                }
+              >
+                <div className="w-[35vw] h-[15vh] rounded-md flex justify-center items-center text-[#262626] text-[1.7vw] font-semibold bg-white hover:cursor-pointer lg:hover:shadow-lg shadow lg:hover:scale-105 lg:hover:-translate-y-2 duration-200 active:scale-90">
+                  {questions.data.questions[currentQuestion].options[3].answer}
+                </div>
+              </button>
+            </div>
+          </div>
+        </>
+      )}
+    </div>
+  );
+};
 
 export default Quiz;
