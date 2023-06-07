135ee91156c51ef9f5348bc7a4ad16f0ae38f515
vzrenggamani <rengga.prakoso@gmail.com>
2023-06-07 14:56:52 +0700

    feat: split componentent and rendering to blog and quiz pages

--- src/pages/blog/index.jsx
+++ src/pages/blog/index.jsx
@@ -1,7 +1,7 @@
+import ArticleCard from "@/components/ArticleCard";
 import HeroBlog from "@/components/HeroBlog";
 import fs from "fs";
 import matter from "gray-matter";
-import Link from "next/link";
 
 export async function getStaticProps() {
   const files = fs.readdirSync("posts");
@@ -31,34 +31,7 @@ export default function Blog({ posts }) {
         <h1 className="text-xl font-bold text-white">Postingan Terbaru</h1>
         <div className="w-full mx-auto container flex flex-wrap">
           {posts.map(({ slug, frontmatter }) => (
-            <Link
-              href={`/blog/${slug}`}
-              key={slug}
-              className="lg:w-1/3 sm:w-1/2 w-full p-3"
-            >
-              <div className="relative h-full rounded-lg border-2 border-gray-300 flex flex-col gap-3 overflow-hidden p-5 text-white">
-                <div className="w-full h-60 bg-[#D9D9D9] rounded"></div>
-                <h2 className="text-[1.5vw] font-semibold">
-                  {frontmatter.title}
-                </h2>
-                <h4>3 Juni 2023</h4>
-                <p>
-                  sinopsis dari artikel ini , tujuan , atau apalah itu pokok
-                  agak panjang atau waktu input diberi input sinopsis, taruh
-                  sini
-                </p>
-                <div className="flex flex-wrap gap-3">
-                  <div className="px-3 py-1 rounded border-2 border-gray-300">
-                    {" "}
-                    Kategori 1
-                  </div>
-                  <div className="px-3 py-1 rounded border-2 border-gray-300">
-                    {" "}
-                    Kategori 2
-                  </div>
-                </div>
-              </div>
-            </Link>
+            <ArticleCard slug={slug} frontmatter={frontmatter} key={slug} />
           ))}
         </div>
       </div>
