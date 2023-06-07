74e32c944ce4f46207eb990c2fdbef3b641ca53e
Muhammad Fahli Saputra <saputra@fahli.net>
2023-06-07 20:19:19 +0800

    implement searching

---
+++ src/pages/api/search.js
@@ -0,0 +1,75 @@
+import fs from "fs";
+import matter from "gray-matter";
+
+export default async function handler(req, res) {
+    if (req.method === "GET") {
+        try {
+            const search = req.query.search;
+
+            if (search === undefined) {
+                res.status(400).json({ message: "Bad request: search parameter is missing" });
+                return;
+            }
+
+            const type = req.query.type;
+
+            if (!(type == "quiz" || type == "blog" || type == "all" || type == "" || type === undefined)) {
+                res.status(400).json({ message: "Bad request: type parameter is invalid. Available: quiz, blog" });
+                return;
+            }
+
+            var search_data = [];
+
+            if (type == "quiz" || type == "all" || type == "" || type === undefined) {
+                const files = fs.readdirSync("quizzes");
+                files.map((fileName) => {
+                    const slug = "/quiz/" + fileName.replace(".json", "");
+                    const readFile = fs.readFileSync(`quizzes/${fileName}`, "utf-8");
+                    const json = JSON.parse(readFile);
+                    const metadata = json.metadata;
+                    if (metadata.title.toLowerCase().includes(search.toLowerCase())) {
+                        const title = metadata.title;
+                        const description = metadata.description;
+
+                        const data = {
+                            slug: slug,
+                            title: title,
+                            description: description
+                        }
+                        search_data.push(data);
+                    }
+                });
+            }
+
+
+            if (type == "blog" || type == "all" || type == "" || type === undefined) {
+                const posts = fs.readdirSync("posts");
+                posts.map((fileName) => {
+                    const slug = "/blog/" + fileName.replace(".md", "");
+                    const readFile = fs.readFileSync(`posts/${fileName}`, "utf-8");
+                    const { data: frontmatter } = matter(readFile);
+
+                    if (frontmatter.title.toLowerCase().includes(search.toLowerCase())) {
+                        const title = frontmatter.title;
+                        const excerpt = frontmatter.excerpt;
+
+                        const data = {
+                            slug: slug,
+                            title: title,
+                            description: excerpt
+                        }
+                        search_data.push(data);
+                    }
+                });
+            }
+
+            // Send a success response
+            res.status(200).json({
+                message: "Data retrieved successfully!",
+                results: search_data
+            });
+        } catch (error) {
+            res.status(500).json({ message: "An error occurred: " + error.message });
+        }
+    }
+}
