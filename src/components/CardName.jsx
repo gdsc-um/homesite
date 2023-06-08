f5c520790b4c7b2e1aa2a58ec0351d017261a8ff
vzrenggamani <rengga.prakoso@gmail.com>
2023-06-08 23:11:06 +0700

    test: for props passing ability

--- src/components/CardName.jsx
+++ src/components/CardName.jsx
@@ -1,15 +1,27 @@
 import Link from "next/link";
 
+/**
+ * @param {object} frontmatter - the frontmatter of the article
+ * @param {string} frontmatter.name - the name of the person
+ * @param {string} frontmatter.role - the role of the person
+ * @param {string} frontmatter.picture - the picture of the person
+ * @param {string} frontmatter.profile_url - the profile url of the person
+ * @returns {JSX.Element} - the article card component
+ */
+
 export default function ArticleCard({ frontmatter }) {
   return (
-    <Link href={`${frontmatter.profile_url}`} className="lg:w-1/3 sm:w-1/2 w-full p-3">
+    <Link
+      href={`${frontmatter.profile_url}`}
+      className="lg:w-1/3 sm:w-1/2 w-full p-3"
+    >
       <div className="relative h-full rounded-lg border-2 border-gray-300 flex flex-col gap-3 overflow-hidden p-5 text-white">
         <div className="w-full h-60  rounded">
-            <img
-                src={`${frontmatter.picture}`}
-                alt="profile"
-                className="w-full h-full object-cover object-center rounded-lg overflow-hidden"
-            />
+          <img
+            src={`${frontmatter.picture}`}
+            alt="profile"
+            className="w-full h-full object-cover object-center rounded-lg overflow-hidden"
+          />
         </div>
         <h2 className="text-m font-semibold">{frontmatter.name}</h2>
         {/* show it's role */}
@@ -18,5 +30,3 @@ export default function ArticleCard({ frontmatter }) {
     </Link>
   );
 }
-
-
