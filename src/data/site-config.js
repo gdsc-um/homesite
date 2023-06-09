2168cc425570ccae91c0d43df909d354a544adb2
vzrenggamani <rengga.prakoso@gmail.com>
2023-06-09 22:04:21 +0700

    chore: added global site config for future deployment and consistent data pulling

---
+++ src/data/site-config.js
@@ -0,0 +1,27 @@
+/* STATIC DATA FOR SITE CONFIG
+ * This file contains static data for site configuration
+ * You can change the value of the data below according to your needs
+ * You can also add or delete the data below according to your needs
+ * Hope this simplifies the way of site global configuration
+ */
+
+export const siteConfig = {
+  siteName: "GDSC UM 2023",
+  siteDescription: "GDSC UM 2023",
+  siteUrl: "https://gdscum2023.github.io",
+  siteImage: "https://gdscum2023.github.io/assets/images/logo.png",
+  siteLanguage: "id",
+  contactEmail: "gdsc.um@gmail.com",
+  contactPhone: "",
+  address:
+    "Jalan Semarang No 5; Universitas Negeri Malang; Malang; Jawa Timur; Indonesia",
+  founded: "2022",
+  author: "GDSC UM 2023",
+  socialMedia: [
+    "https://www.facebook.com/gdscum2023",
+    "https://www.instagram.com/gdscum2023",
+    "https://www.linkedin.com/company/gdscum2023",
+  ],
+  themeColor: "#000000",
+  backgroundColor: "#ffffff",
+};
