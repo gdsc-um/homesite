95f43e124bda6540b35a5c879e1b79bcdcfa563c
vzrenggamani <rengga.prakoso@gmail.com>
2023-06-13 13:45:33 +0700

    refactor(ui): minor style fix and added cover image on top of the contents

--- src/pages/blog/[slug].jsx
+++ src/pages/blog/[slug].jsx
@@ -1,5 +1,7 @@
+import IMAGEPLACEHOLDER from "@/assets/placeholder.jpg";
 import fs from "fs";
 import matter from "gray-matter";
+import Image from "next/image";
 import ReactMarkdown from "react-markdown";
 
 export async function getStaticPaths() {
@@ -30,8 +32,39 @@ export async function getStaticProps({ params: { slug } }) {
 export default function Artikel({ frontmatter, content }) {
   return (
     <div className="w-full py-10">
-      <article className="container mx-auto prose-full text-white">
-        <h1>{frontmatter.title}</h1>
+      <article className="container mx-auto prose-full prose-lg text-white">
+        {/* check if frontmatter available, if some random image */}
+        {!frontmatter.cover ? (
+          <div className="overflow-hidden">
+            <Image
+              src={IMAGEPLACEHOLDER}
+              alt=""
+              className="w-full h-[720px] object-cover rounded-lg"
+              width={1280}
+              height={720}
+            />
+          </div>
+        )
+          : (
+            <div className="overflow-hidden">
+              <Image
+                src={frontmatter.cover}
+                alt=""
+                className="w-full h-[720px] object-cover rounded-lg"
+                width={1280}
+                height={720}
+              />
+            </div>
+          )}
+        <h1 className="text-4xl font-bold pt-8 mb-3">{frontmatter.title}</h1>
+        {/* render date in Indonesian locale */}
+        <p className="text-gray-400 text-md my-0">
+          {new Date(frontmatter.date).toLocaleDateString("id-ID", {
+            day: "numeric",
+            month: "long",
+            year: "numeric",
+          })}
+        </p>
         <ReactMarkdown>{content}</ReactMarkdown>
       </article>
     </div>
