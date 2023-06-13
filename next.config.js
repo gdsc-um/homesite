1540d1584e0f2730d19b9710e43f94e0b10fd4b6
vzrenggamani <rengga.prakoso@gmail.com>
2023-06-13 13:42:34 +0700

    build: added cloudinary and fahli remote image domain

--- next.config.js
+++ next.config.js
@@ -1,4 +1,9 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  reactStrictMode: true,
+  images: {
+    domains: ["localhost", "res.cloudinary.com", "fahli.net"],
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
